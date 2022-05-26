176c6afcf9b0498ad05978b4074d675ad5820782
felix-blueinno <94157332+felix-blueinno@users.noreply.github.com>
2022-05-26 19:20:52 +0800

    completed login ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,13 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
-import 'dart:io';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_jeffrey_dev/event_provider.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
-
 import 'calendar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -19,79 +15,203 @@ main() async {
   runApp(ChangeNotifierProvider(
     create: (context) => EventProvider(),
     child: MaterialApp(
-      routes: {
-        '/calendar': (context) => CalendarPage(),
-      },
+      routes: {'/calendar': (context) => CalendarPage()},
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: LoginPage(),
       theme: ThemeData.dark().copyWith(useMaterial3: true),
     ),
   ));
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  Image image = Image.network(
-    'https://picsum.photos/id/0/5616/3744',
-    fit: BoxFit.fill,
-  );
+class _LoginPageState extends State<LoginPage> {
+  /// ref: https://dribbble.com/shots/18219801-Mobile-App-Login-Signup
+
+  bool _visiblePw = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            flex: 20,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    'Time: ',
-                    style: TextStyle(fontSize: 48),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: FlutterLogo(
+                  size: 200,
+                ),
+              ),
+
+              /// Email
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'hello@example.com',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+
+              SizedBox(height: 32),
+
+              /// Password
+              TextField(
+                obscureText: _visiblePw,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Your password',
+                  border: OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    onPressed: () => setState(() => _visiblePw = !_visiblePw),
+                    icon: Icon(
+                      _visiblePw ? Icons.visibility : Icons.visibility_off,
+                    ),
                   ),
                 ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: Text(DateTime.now().toString()),
+              ),
+
+              SizedBox(height: 8),
+
+              /// Forgot password
+              Align(
+                alignment: Alignment.centerLeft,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text('Forgot Password?'),
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
                 ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 60,
-            child: GestureDetector(
-              onTap: () async {
-                ImagePicker _picker = ImagePicker();
-                XFile? pickedImage =
-                    await _picker.pickImage(source: ImageSource.gallery);
+              ),
 
-                if (pickedImage != null) {
-                  if (kIsWeb) {
-                    var bytes = await pickedImage.readAsBytes();
-                    image = Image.memory(bytes);
-                  } else {
-                    var file = File(pickedImage.path);
-                    image = Image.file(file);
-                  }
+              SizedBox(height: 32),
 
-                  setState(() {});
-                }
-              },
-              child: image,
-            ),
+              /// Login button
+              CoverPageButton(
+                child: Text('Login',
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold)),
+                onTap: () {},
+              ),
+
+              SizedBox(height: 8),
+
+              /// Sign up button
+              CoverPageButton(
+                backgroundColor: Colors.transparent,
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+                onTap: () {},
+              ),
+
+              SizedBox(height: 16),
+
+              Row(
+                children: [
+                  Expanded(child: Divider(color: Colors.grey, height: 2)),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text('OR', style: TextStyle(color: Colors.grey)),
+                  ),
+                  Expanded(child: Divider(color: Colors.grey, height: 2)),
+                ],
+              ),
+
+              SizedBox(height: 16),
+
+              // Continue with Google button
+              CoverPageButton(
+                onTap: () {},
+                backgroundColor: Color.fromRGBO(0, 0, 0, 0),
+                child: Row(
+                  children: [
+                    Image.network(
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2048px-Google_%22G%22_Logo.svg.png',
+                      height: 24,
+                    ),
+                    Spacer(),
+                    Text(
+                      'Continue with Google',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Spacer(),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 16),
+
+              // Continue anonymously button
+              CoverPageButton(
+                onTap: () {},
+                backgroundColor: Colors.transparent,
+                child: Row(
+                  children: [
+                    Icon(Icons.person, color: Colors.white),
+                    Spacer(),
+                    Text('Continue anonymously',
+                        style: TextStyle(
+                          color: Colors.white,
+                        )),
+                    Spacer(),
+                  ],
+                ),
+              ),
+            ],
           ),
-          ElevatedButton(
-            onPressed: () => Navigator.pushNamed(context, '/calendar'),
-            child: Text('Calendar'),
+        ),
+      ),
+    );
+  }
+}
+
+class CoverPageButton extends StatelessWidget {
+  final Function()? onTap;
+  final Widget child;
+  final Color? backgroundColor;
+  final Color? borderColor;
+  final double borderRadius;
+  final double widthFactor;
+
+  const CoverPageButton({
+    Key? key,
+    required this.onTap,
+    required this.child,
+    this.backgroundColor = Colors.white,
+    this.borderColor = Colors.grey,
+    this.borderRadius = 5,
+    this.widthFactor = 1,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: widthFactor,
+      child: OutlinedButton(
+        onPressed: () {},
+        child: child,
+        style: OutlinedButton.styleFrom(
+          backgroundColor: backgroundColor,
+          side: BorderSide(color: borderColor!),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius),
           ),
-        ],
+        ),
       ),
     );
   }
